edddf0760c42c3ba96eebbb74dc2d5f8fb63f61a
xdf <18758007456@163.com>
2017-09-11 20:55:51 +0800

    first commit

--- src/QueenAndStack/MaxArr.java
+++ src/QueenAndStack/MaxArr.java
@@ -1,42 +1,58 @@
 package QueenAndStack;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017/9/7 0007.
  */
 public class MaxArr {
-    public int[] maxArr(int[] arr, int w){
-        int len = arr.length - w + 1;
-        int[] result = new int[len];
+    private List<Integer> selectMax(int[] arr,int width){
+        Deque<Integer> deque = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        int start = 0;
+        int len = arr.length;
         for (int i = 0; i < len; i++) {
-            int[] threeNum = {arr[i], arr[i + 1], arr[i + 2]};
-            result[i] = selectMax(threeNum);
+            if (deque.isEmpty()) {
+                deque.add(i);
+            } else {
+                if (arr[deque.getLast()] >= arr[i]) {
+                    deque.addLast(i);
+                } else {
+                    while (arr[deque.getLast()] < arr[i]) {
+                        deque.pollLast();
+                        if (deque.isEmpty()) {
+                            break;
+                        }
+                    }
+                    deque.addLast(i);
+                }
+            }
+            if (i - start == width - 1) {
+                start++;
+                result.add(maxInWin(deque, arr));
+            }
+            if (deque.getLast() -  deque.getFirst() == width) {
+                deque.pollFirst();
+            }
+
         }
         return result;
     }
-    private int selectMax(int[] threeNum){
-        int a = threeNum[0];
-        int b = threeNum[1];
-        int c = threeNum[2];
-        if(a > b){
-            if(a > c){
-                return a;
-            }else{
-                return c;
-            }
-        }else{
-            if(b > c){
-                return b;
-            }else{
-                return c;
+
+    private int maxInWin(Deque<Integer> deque, int[] arr) {
+        int temp = Integer.MIN_VALUE;
+        int result = -1;
+        for (Integer x: deque) {
+            if (temp < arr[x]) {
+                temp = arr[x];
+                result = x;
             }
         }
+        return result;
     }
-
     public static void main(String[] args) {
         MaxArr maxArr = new MaxArr();
         int[] arr = {4,3,5,4,3,3,6,7};
-        System.out.println(Arrays.toString(maxArr.maxArr(arr, 3)));
+        System.out.println(maxArr.selectMax(arr, 3));
     }
 }
