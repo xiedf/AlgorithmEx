711b7d44c3a07cf2083a38fa66c3e6e808bc9f46
xdf <18758007456@163.com>
2017-09-11 21:22:50 +0800

    first commit

---
+++ src/LinkedList/PrintCommonPart.java
@@ -0,0 +1,46 @@
+package LinkedList;
+
+/**
+ * Created by Administrator on 2017/9/11 0011.
+ */
+class Node {
+    public int value;
+    public Node next;
+    public Node(int data) {
+        this.value = data;
+    }
+}
+
+public class PrintCommonPart {
+    public void printCommonLinkedList(Node head1, Node head2) {
+        while (head1 != null & head2 != null) {
+            if (head1.value > head2.value) {
+                head2 = head2.next;
+            } else if (head1.value < head2.value) {
+                head1 = head1.next;
+            } else {
+                System.out.println(head1.value);
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Node l1 = new Node(0);
+        Node l2 = new Node(3);
+        Node head1 = l1;
+        Node head2 = l2;
+        for (int i = 1; i < 9; i++) {
+            l1.next = new Node(i);
+            l1 = l1.next;
+        }
+        for (int i = 4; i < 12; i++) {
+            l2.next = new Node(i);
+            l2 = l2.next;
+        }
+        PrintCommonPart p = new PrintCommonPart();
+        p.printCommonLinkedList(head1, head2);
+    }
+}
+
